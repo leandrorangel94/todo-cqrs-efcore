a8022f1a95b15b25f05a94ad15c82d08d284306c
Leandro Rangel <rangel94@outlook.com>
2023-11-04 19:21:42 -0300

    Estrutura do Projeto e criação de Entities

---
+++ Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+namespace Todo.Domain.Entities
+{
+  public abstract class Entity : IEquatable<Entity>
+  {
+    public Entity()
+    {
+      Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; private set; }
+
+    public bool Equals(Entity? other)
+    {
+      if (other != null)
+        return Id == other.Id;
+      return false;
+    }
+  }
+}
