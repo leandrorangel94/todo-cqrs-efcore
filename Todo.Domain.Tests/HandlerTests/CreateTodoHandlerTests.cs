12ac1b91c56f34c9356c1c6fd02dd4c415cc1b7a
Leandro Rangel <rangel94@outlook.com>
2023-11-06 14:29:07 -0300

    Testes do Handler

--- Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
+++ Todo.Domain.Tests/HandlerTests/CreateTodoHandlerTests.cs
@@ -1,23 +1,31 @@
 using Todo.Domain.Handlers;
 using Todo.Domain.Commands;
+using Todo.Domain.Tests.Repositories;
 
 namespace Todo.Domain.Tests.HandlerTests
 {
   [TestClass]
   public class CreateTodoHandlerTests
   {
+    private readonly CreateTodoCommand _invalidCommand = new CreateTodoCommand("", "", DateTime.Now);
+    private readonly CreateTodoCommand _validCommand = new CreateTodoCommand("Titulo da tarefa", "leandrorangel", DateTime.Now);
+    private readonly TodoHandler _handler = new TodoHandler(new FakeTodoRepository());
+    private GenericCommandResult _result = new GenericCommandResult();
+    public CreateTodoHandlerTests()
+    {
+    }
     [TestMethod]
     public void Dado_um_comando_invalido_deve_interromper_a_execucao()
     {
-      var command = new CreateTodoCommand("", "", DateTime.Now);
-      var handler = new TodoHandler(null);
-      Assert.Fail();
+      _result = (GenericCommandResult)_handler.Handle(_invalidCommand);
+      Assert.AreEqual(_result.Success, false);
     }
 
     [TestMethod]
     public void Dado_um_comando_valido_deve_criar_tarefa()
     {
-      Assert.Fail();
+      _result = (GenericCommandResult)_handler.Handle(_validCommand);
+      Assert.AreEqual(_result.Success, true);
     }
   }
 }
