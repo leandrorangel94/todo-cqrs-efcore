1ba2e079813cc6b7770026c19c37ec69cb070ae4
Leandro Rangel <rangel94@outlook.com>
2023-11-06 09:56:42 -0300

    Red Green Refactor - Testes de Unidade

---
+++ Todo.Domain.Tests/CommandTests/CreateTodoCommandTests.cs
@@ -0,0 +1,17 @@
+namespace Todo.Domain.Tests.CommandTests;
+
+[TestClass]
+public class CreateTodoCommandTests
+{
+    [TestMethod]
+    public void Dado_um_comando_invalido()
+    {
+        Assert.Fail();
+    }
+
+    [TestMethod]
+    public void Dado_um_comando_valido()
+    {
+        Assert.Fail();
+    }
+}
