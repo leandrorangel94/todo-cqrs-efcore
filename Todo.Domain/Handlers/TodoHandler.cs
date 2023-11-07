f339439ab7a98ab46d5ccaca1daeeaed49584974
Leandro Rangel <rangel94@outlook.com>
2023-11-07 10:05:16 -0300

    Instalação Entity Framework Core

--- Todo.Domain/Handlers/TodoHandler.cs
+++ Todo.Domain/Handlers/TodoHandler.cs
@@ -7,11 +7,14 @@ using Todo.Domain.Repositories;
 
 namespace Todo.Domain.Handlers
 {
-  public class TodoHandler : Notifiable, IHandler<CreateTodoCommand>, IHandler<UpdateTodoCommand>
+  public class TodoHandler : Notifiable,
+  IHandler<CreateTodoCommand>,
+  IHandler<UpdateTodoCommand>,
+  IHandler<MarkTodoAsDoneCommand>,
+  IHandler<MarkTodoAsUndoneCommand>
   {
 
     private readonly ITodoRepository _repository;
-
     public TodoHandler(ITodoRepository repository)
     {
       _repository = repository;
@@ -32,7 +35,6 @@ namespace Todo.Domain.Handlers
       //Retorna o resultado
       return new GenericCommandResult(true, "Tarefa salva!", todo);
     }
-
     public ICommandResult Handle(UpdateTodoCommand command)
     {
       // Fail Fast Validation
@@ -53,5 +55,35 @@ namespace Todo.Domain.Handlers
       return new GenericCommandResult(true, "Tarefa salva", DateTime.Now);
 
     }
+    public ICommandResult Handle(MarkTodoAsDoneCommand command)
+    {
+      command.Validate();
+      if (command.Invalid)
+        return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+      var todo = _repository.GetById(command.Id, command.User);
+
+      todo.MarkAsDone();
+
+      _repository.Update(todo);
+
+      return new GenericCommandResult(true, "Tarefa salva", DateTime.Now);
+    }
+    public ICommandResult Handle(MarkTodoAsUndoneCommand command)
+    {
+      command.Validate();
+      if (command.Invalid)
+        return new GenericCommandResult(false, "Ops, parece que sua tarefa está errada!", command.Notifications);
+
+      var todo = _repository.GetById(command.Id, command.User);
+
+      todo.MarkAsUndone();
+
+      _repository.Update(todo);
+
+      return new GenericCommandResult(true, "Tarefa salva", DateTime.Now);
+    }
   }
+
+
 }
