f6915bbb7a05194420efa6ac76309b0d51329cdd
Leandro Rangel <rangel94@outlook.com>
2023-11-07 08:57:20 -0300

    Teste de Entidade

---
+++ Todo.Domain.Tests/EntityTests/TodoItemTests.cs
@@ -0,0 +1,15 @@
+using Todo.Domain.Entities;
+
+namespace Todo.Domain.Tests.EntityTests
+{
+  [TestClass]
+  public class TodoItemTests
+  {
+    private readonly TodoItem _validTodo = new TodoItem("Titulo da tarefa", "leandrorangel", DateTime.Now);
+    [TestMethod]
+    public void Dado_um_novo_todo_o_mesmo_nao_pode_ser_concluido()
+    {
+      Assert.AreEqual(_validTodo.Done, false);
+    }
+  }
+}
