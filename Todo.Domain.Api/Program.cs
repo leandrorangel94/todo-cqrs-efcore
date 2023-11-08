11fc8427e4d2075c9900856c417f62d77ccca7a2
Leandro Rangel <rangel94@outlook.com>
2023-11-07 22:20:18 -0300

    Estrutura do Repositorio

--- Todo.Domain.Api/Program.cs
+++ Todo.Domain.Api/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.EntityFrameworkCore;
+using Todo.Domain.Handlers;
 using Todo.Domain.Infra.Contexts;
+using Todo.Domain.Infra.Repositories;
+using Todo.Domain.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
 // builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("")));
+
+builder.Services.AddTransient<ITodoRepository, TodoRepository>();
+builder.Services.AddTransient<TodoHandler, TodoHandler>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- Todo.Domain/Repositories/ITodoRepository.cs
+++ Todo.Domain/Repositories/ITodoRepository.cs
@@ -8,5 +8,9 @@ namespace Todo.Domain.Repositories
     void Update(TodoItem todo);
 
     TodoItem GetById(Guid id, string user);
+    IEnumerable<TodoItem> GetAll(string user);
+    IEnumerable<TodoItem> GetAllDone(string user);
+    IEnumerable<TodoItem> GetAllUndone(string user);
+    IEnumerable<TodoItem> GetByPeriod(string user, DateTime date, bool done);
   }
 }
