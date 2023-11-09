8a7bc8f3d1ad0eadaf7af0ea2aedd0dc7515454b
Leandro Rangel <rangel94@outlook.com>
2023-11-08 22:19:29 -0300

    Finalização da Controller

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -19,6 +19,66 @@ namespace Todo.Api.Controllers
       return repository.GetAll("leandrorangel");
     }
 
+    [Route("done")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllDone(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetAllDone(user);
+    }
+
+    [Route("done/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetDoneForToday(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetByPeriod("leandrorangel", true, DateTime.Now.Date);
+    }
+
+    [Route("done/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetDoneForTomorrow(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetByPeriod("leandrorangel", true, DateTime.Now.Date.AddDays(1));
+    }
+
+    [Route("undone")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetAllUndone(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetAllUndone(user);
+    }
+
+    [Route("undone/today")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetUndoneForToday(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetByPeriod("leandrorangel", false, DateTime.Now.Date);
+    }
+
+    [Route("undone/tomorrow")]
+    [HttpGet]
+    public IEnumerable<TodoItem> GetUndoneForTomorrow(
+      [FromServices] ITodoRepository repository
+    )
+    {
+      var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return repository.GetByPeriod("leandrorangel", false, DateTime.Now.Date.AddDays(1));
+    }
+
 
     [Route("")]
     [HttpPost]
@@ -28,6 +88,28 @@ namespace Todo.Api.Controllers
     )
     {
       command.User = "leandrorangel";
+      return (GenericCommandResult)handler.Handle(command);
+    }
+
+    [Route("mark-as-done")]
+    [HttpPut]
+    public GenericCommandResult MarkAsDone(
+           [FromBody] MarkTodoAsDoneCommand command,
+           [FromServices] TodoHandler handler
+       )
+    {
+      command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+      return (GenericCommandResult)handler.Handle(command);
+    }
+
+    [Route("mark-as-undone")]
+    [HttpPut]
+    public GenericCommandResult MarkAsUndone(
+           [FromBody] MarkTodoAsDoneCommand command,
+           [FromServices] TodoHandler handler
+       )
+    {
+      command.User = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
       return (GenericCommandResult)handler.Handle(command);
     }
   }
